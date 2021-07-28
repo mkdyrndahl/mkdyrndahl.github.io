351084aaeadeb2425d034ddf80d7c52606c954b9
Matt Dyrndahl <mkdyrndahl@gmail.com>
2021-07-28 14:01:23 -0700

    Reverted chabges to all components, created new project directory

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header/Header'
+import Footer from './Components/Footer/Footer'
+import Hero from './Components/Hero/Hero'
+import About from './Components/About/About'
+import Contact from './Components/Contact/Contact'
+import Portfolio from './Components/Portfolio/Portfolio'
+import Resume from './Components/Resume/Resume'
 
 function App() {
   return (
