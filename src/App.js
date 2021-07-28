974e0081df3147101fabb12afbdc23edc7252487
Matt Dyrndahl <mkdyrndahl@gmail.com>
2021-07-28 14:53:55 -0700

    Added boilerplate, created components for each available on bootstrap template

--- src/App.js
+++ src/App.js
@@ -1,30 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 import Header from './Components/Header/Header'
 import Footer from './Components/Footer/Footer'
-import Hero from './Components/Hero/Hero'
+import Nav from './Components/Nav/Nav'
+import Services from './Components/Services/Services'
 import About from './Components/About/About'
-import Contact from './Components/Contact/Contact'
+import Callout from './Components/Callout/Callout'
+import CallToAction from './Components/CallToAction/CallToAction'
 import Portfolio from './Components/Portfolio/Portfolio'
-import Resume from './Components/Resume/Resume'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" id="root">
+      <Nav/>
+      <Header/>
+      <About/>
+      <Services/>
+      <Callout/>
+      <Portfolio/>
+      <CallToAction/>
+      <Footer/>
     </div>
   );
 }
