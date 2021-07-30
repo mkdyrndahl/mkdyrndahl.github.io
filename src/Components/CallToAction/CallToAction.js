7e415c0f557a11aead0c0af50c81b9404dc3edde
Matt Dyrndahl <mkdyrndahl@gmail.com>
2021-07-30 12:51:09 -0700

    Edited content in CallToAction component, modified package.json to ready for deployment

--- src/Components/CallToAction/CallToAction.js
+++ src/Components/CallToAction/CallToAction.js
@@ -4,9 +4,9 @@ export default function CallToAction()  {
     return(
     <section class="content-section bg-primary text-white">
         <div class="container px-4 px-lg-5 text-center">
-            <h2 class="mb-4">The buttons below are impossible to resist...</h2>
-            <a class="btn btn-xl btn-light me-4" href="#!">Click Me!</a>
-            <a class="btn btn-xl btn-dark" href="#!">Look at Me!</a>
+            <h2 class="mb-4">Find me on GitHub and LinkedIn!</h2>
+            <a class="btn btn-xl btn-light me-4" href="https://github.com/mkdyrndahl">GitHub</a>
+            <a class="btn btn-xl btn-dark" href="https://www.linkedin.com/in/matthew-dyrndahl-944539218/">LinkedIn</a>
         </div>
     </section>
     )
